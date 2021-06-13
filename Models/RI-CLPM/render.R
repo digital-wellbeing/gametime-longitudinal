0e207b241144475a0b0c4e3663e0c8456a0cceb3
Kristoffer <1629860+rpsychologist@users.noreply.github.com>
2021-06-13 18:07:46 +0200

    add render html file

---
+++ Models/RI-CLPM/render.R
@@ -0,0 +1,32 @@
+ OUTPUT_DIR <- "Models/RI-CLPM/html"
+
+# RICLPM example ----
+rmarkdown::render(
+     "Models/RI-CLPM/RICLPM-lavaan-example.Rmd",
+     output_dir = OUTPUT_DIR,
+     encoding = "UTF-8"
+)
+
+# DGP tests ----
+rmarkdown::render(
+     "Models/RI-CLPM/CLPM-lavaan-simulation.Rmd",
+     output_dir = OUTPUT_DIR,
+     encoding = "UTF-8"
+)
+rmarkdown::render(
+     "Models/RI-CLPM/RICLPM-lavaan-simulation.Rmd",
+     output_dir = OUTPUT_DIR,
+     encoding = "UTF-8"
+)
+
+## WIP: brms, multilevel version ----
+rmarkdown::render(
+     "Models/RI-CLPM/CLPM-brms-simulation.Rmd",
+     output_dir = OUTPUT_DIR,
+     encoding = "UTF-8"
+)
+rmarkdown::render(
+     "Models/RI-CLPM/RICLPM-brms-simulation.Rmd",
+     output_dir = OUTPUT_DIR,
+     encoding = "UTF-8"
+)
