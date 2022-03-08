fd5a8def0329b2899036b21700df8f434750e5a8
Matti Vuorre <vuorre@gmail.com>
2022-03-08 09:32:26 +0000

    Update gitignore and common opts

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,2 @@
 source("renv/activate.R")
+source("_common.R")
--- _common.R
+++ _common.R
@@ -1,4 +1,6 @@
 # Set global options here
+library(showtext)
+library(ggplot2)
 
 # Global knitr options
 knitr::opts_chunk$set(
@@ -12,3 +14,19 @@ knitr::opts_chunk$set(
   dev = "ragg_png",
   fig.retina = 2
 )
+
+# Plotting options
+Font <- "Titillium Web"
+font_add_google(Font, Font)
+theme_set(
+  theme_linedraw(
+    base_family = Font,
+    base_size = 9
+  ) +
+    theme(
+      panel.grid.minor = element_blank(),
+      panel.grid.major.x = element_blank()
+    )
+)
+col1 <- "#2980b9"
+W <- 20  # Figure width in cm
